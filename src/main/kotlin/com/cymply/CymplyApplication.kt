e66d7b32730ef0385716864d5839456b54585cbd
simhw <simhw96@gmail.com>
2025-05-20 02:59:45 +0900

    프로젝트 초기 세팅

---
+++ src/main/kotlin/com/cymply/CymplyApplication.kt
@@ -0,0 +1,11 @@
+package com.cymply
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CymplyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CymplyApplication>(*args)
+}
