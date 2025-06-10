4f20bed3c07cab59cbf4dc599abea6b0e4ede24f
god3v <sooon9254@naver.com>
2025-06-10 23:12:34 +0900

    chore: Spring Application Profiles 분리 :memo: 로컬에서 실행 시 application-local.yml 파일 생성 후 실행 권장

---
+++ src/main/kotlin/com/cymply/music/adapter/in/web/dto/SearchMusicRequest.kt
@@ -0,0 +1,16 @@
+package com.cymply.music.adapter.`in`.web.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "음악 검색 요청 DTO")
+data class SearchMusicRequest(
+
+    @Schema(description = "검색 키워드", example = "좋은 날")
+    val keyword: String,
+
+    @Schema(description = "페이지 내 트랙 개수", example = "30")
+    val limit: Int = 30,
+
+    @Schema(description = "페이지 번호", example = "1")
+    val page: Int = 1
+)
--- src/main/kotlin/com/cymply/music/adapter/in/web/dto/SearchMusicResponse.kt
+++ src/main/kotlin/com/cymply/music/adapter/in/web/dto/SearchMusicResponse.kt
@@ -1,12 +1,13 @@
 package com.cymply.music.adapter.`in`.web.dto
 
+import com.cymply.music.application.port.dto.SearchMusicResult
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "음악 상세 응답 DTO")
 data class MusicResponse(
 
     @field:Schema(description = "음악 ID", example = "1000")
-    val id: Long,
+    val id: Long?,
 
     @field:Schema(description = "음악 제목", example = "좋은 날")
     val title: String,
@@ -16,4 +17,14 @@ data class MusicResponse(
 
     @field:Schema(description = "음악 썸네일 url", example = "www.example.com")
     val thumbnail: String,
-)
+) {
+    companion object {
+        fun of(result: SearchMusicResult): MusicResponse =
+            MusicResponse(
+                id = null,
+                title = result.title,
+                artist = result.artist,
+                thumbnail = result.thumbnail
+            )
+    }
+}
